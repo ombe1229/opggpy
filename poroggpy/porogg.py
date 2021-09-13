abac8ace7205f63d85984b4ef79cf0d0541827f4
ombe <h3236516@gmail.com>
2021-09-14 02:31:43 +0900

    refactor(porogg): refactoring ch_sr

--- poroggpy/porogg.py
+++ poroggpy/porogg.py
@@ -1,13 +1,18 @@
+from typing import Literal, Optional
+
+from aiohttp.client import ClientSession
 from poroggpy.http import PoroggHttpClient
 from poroggpy.parser import PoroggParser
 from poroggpy.models.champion_sr import ChampionSr
 
 
-class Porogg:
-    def __init__(self) -> None:
-        self.http = PoroggHttpClient()
+class Porogg(PoroggHttpClient):
+    def __init__(self, session: Optional[ClientSession] = None) -> None:
+        super().__init__(session)
         self.parser = PoroggParser()
 
-    async def get_ch_build(self, champion, lane):
-        html = await self.http.get_ch_sr(champion, lane)
-        return ChampionSr(await self.parser.parse_ch_sr(html))
+    async def ch_sr(
+        self, champion: str, lane: Literal["top", "jng", "mid", "adc", "sup"]
+    ) -> ChampionSr:
+        html = await self.get_ch_sr(champion, lane)
+        return ChampionSr(self.parser.parse_ch_sr(html))
